d52b07054b61f32231404c1a6d96e2389660a331
Djalma Araujo <djalma.araujo@gmail.com>
2015-07-13 18:31:35 -0300

    #20 - Add Charset Option to Request

--- lib/pagseguro/request.rb
+++ lib/pagseguro/request.rb
@@ -14,13 +14,17 @@ module PagSeguro
 
     def post(path, version, account = "default", params = {})
       self.class.base_uri PagSeguro.api_url(version)
+
       options = { body: add_credencials(account) }
       options[:body].merge!(params)
       options[:timeout] = PagSeguro.timeout unless PagSeguro.timeout.blank?
+      options[:headers][:charset] = PagSeguro.charset if  PagSeguro.charset
+
       self.class.post(path, options)
     end
 
     private
+
     def add_credencials(account)
       if account == "alternative"
          { email: PagSeguro.alt_email, token: PagSeguro.alt_token }
